646161696353a84dcddbed502e2e8228263bdbd9
daniandp <andradegsp@gmail.com>
2022-05-17 17:38:30 -0500

    En el archivo validator.js cree: *La variable creditcardnumber *Las funciones isvalid y maskify *Todo funciona sin errores hasta aquí

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,11 @@
+let creditCardNumber= RegExp('^[0-9]{16}$"')
 const validator = {
-  // ...
+
+isvalid:function(creditCardNumber){},
+maskify:function (creditCardNumber){},
+ 
+ 
+  
 };
 
 export default validator;
